2a96356029b5c92608536c57934cc8fddcf8c752
manaver <vermamanav117@gmail.com>
2024-01-07 15:25:26 +0530

    Update: Rustlings init

---
+++ exercises/intro/intro2.rs
@@ -0,0 +1,11 @@
+// intro2.rs
+//
+// Make the code print a greeting to the world.
+//
+// Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
+// hint.
+
+fn main() {
+    let hello_world: String = "Hello World!".to_string();
+    println!("Hello {}!", hello_world);
+}
